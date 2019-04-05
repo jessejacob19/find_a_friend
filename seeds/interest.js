f43445c468af3e54bdd8ed7c0c8017a7107b68e5
Nanz Nair <nanz89@gmail.com>
2019-04-05 15:15:47 +1300

    image fix

--- seeds/interest.js
+++ seeds/interest.js
@@ -6,14 +6,14 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('interest').insert([
         {id: 1, interest: 'basketball', image: '/basketball.jpg'},
-        {id: 2, interest: 'photography', image: '/dancing.jpg'},
-        {id: 3, interest: 'dancing', image: '/gaming.jpg'},
-        {id: 4, interest: 'writing', image: '/geocaching.jpg'},
+        {id: 2, interest: 'photography', image: '/photography.jpg'},
+        {id: 3, interest: 'dancing', image: '/dancing.jpg'},
+        {id: 4, interest: 'writing', image: '/writing.jpg'},
         {id: 5, interest: 'hiking', image: '/hiking.jpg'},
-        {id: 6, interest: 'racing', image: '/photography.jpg'},
-        {id: 7, interest: 'gaming', image: '/racing.jpg'},
-        {id: 8, interest: 'geocaching', image: '/scrapbooking.jpg'},
-        {id: 9, interest: 'scrapbooking', image: '/writing.jpg'}
+        {id: 6, interest: 'racing', image: '/racing.jpg'},
+        {id: 7, interest: 'gaming', image: '/gaming.jpg'},
+        {id: 8, interest: 'geocaching', image: '/geocaching.jpg'},
+        {id: 9, interest: 'scrapbooking', image: '/scrapbooking.jpg'}
       ]);
     });
 };
